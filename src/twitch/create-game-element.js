c0b404376760083b14e04444fba028f92b7027fa
Alex T Leach <alextleach@gmail.com>
2016-07-07 15:23:47 -0500

    got the games to show up

--- src/twitch/create-game-element.js
+++ src/twitch/create-game-element.js
@@ -26,5 +26,46 @@
 // }
 
 export default function (game) {
+  // this creates a parent element
+  const gameElement = document.createElement('div');
+  // this just adds a class name to the parent
+  gameElement.classList.add('game-item');
+
+  // this creates an h3 to be used for game title
+  const gameName = document.createElement('h3');
+  // this adds a class to the h3 we just created
+  gameName.classList.add('game-item__name');
+  // adds h3 to Parent (div called gameElement)
+  gameElement.appendChild(gameName);
+  // this grabs the name to put in the h3
+  gameName.innerText=game.name;
+
+
+  //creates a new h4 called gamePop for popularity
+  const gamePop = document.createElement('h4');
+  // and then adds a class
+  gamePop.classList.add('game-item__popularity');
+  //adds the new h4 to the first div we created
+  gameElement.appendChild(gamePop);
+  //grabs popularity to put in the new h4
+  gamePop.innerText = game.popularity;
+
+  //creates a new image element called gameCover
+  const gameCover= document.createElement('img');
+  //gives the image a class name
+  gameCover.classList.add('game-item__pic');
+  //grabs the image's alt name
+  gameCover.alt = game.name;
+  //grabs the large image cover
+  gameCover.src=game.box.large;
+  //adds the new cover the the first div
+  gameElement.appendChild(gameCover);
+
+
+
+
+
+  // console.log(gameElement);
+  return gameElement;
 
 }
--- src/twitch/search-twitch.js
+++ src/twitch/search-twitch.js
@@ -9,15 +9,22 @@
 // parentElement with the games that it finds!
 
 import getTwitchData from './get-data';
+import addGamesToList from './add-games-to-list';
 
 export default function (parentElement, searchTerm, getData = getTwitchData) {
   // This function will run after `getData` has received data
   // Games will be a list of games from the twitch results
   const afterData = function (games) {
 
-  };
+    addGamesToList(parentElement, games);
+      // parentElement.innerHTML = '';
+       const newList = document.createElement('ul');
+       parentElement.appendChild(newList);
+       newList.classList.add('game-item');
+       console.log(parentElement)
+     };
 
-  // Need to return this for the tests to work...
-  // More on that next week!
-  return getData().then(afterData);
-}
+     // Need to return this for the tests to work...
+     // More on that next week!
+     return getData(searchTerm).then(afterData);
+   }
